80a0a52bf16acd5d7a6189b5742d254b356fae09
zerai <teclaizerai@gmail.com>
2023-09-06 11:32:16 +0200

    Apply php doc headers.

--- src/Application/ModuleFinder.php
+++ src/Application/ModuleFinder.php
@@ -1,5 +1,19 @@
 <?php declare(strict_types=1);
 
+/*
+ * This file is part of the medicalmundi/oe-module-marketplace
+ *
+ * @copyright (c) Zerai Teclai <teclaizerai@gmail.com>.
+ * @copyright (c) Francesca Bonadonna <francescabonadonna@gmail.com>.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * {@link https://github.com/medicalmundi/oe-module-marketplace/graphs/contributors developer} and is licensed under the MIT license.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * @license https://github.com/MedicalMundi/oe-module-marketplace/blob/main/LICENSE MIT
+ */
+
 namespace OpenEMR\Modules\Marketplace\Application;
 
 interface ModuleFinder
